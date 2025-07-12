9b591fd77531f163cf03f2b655eaff80b862c96c
sathish vinayk <sathishvinayk@gmail.com>
2025-07-12 18:52:54 +0530

    Initial Commit

---
+++ StealthOverflow/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  StealthOverflow
+//
+//  Created by Sathishvinayk on 12/07/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
